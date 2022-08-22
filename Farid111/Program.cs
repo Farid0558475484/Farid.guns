55b504850392c76bea083e50d9c5ee42d045b45d
Farid0558475484 <faridmh@code.edu.az>
2022-08-22 19:01:02 +0400

    first t

---
+++ Farid111/Program.cs
@@ -0,0 +1,114 @@
+﻿
+using System;
+
+
+namespace Farid111
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            Console.WriteLine("MagazineCapacity");
+            int MagazineCapacity = int.Parse(Console.ReadLine());
+            Console.WriteLine("MemberOfBulletsRemaining");
+            int MemberOfBulletsRemaining = int.Parse(Console.ReadLine());
+            Console.WriteLine("DischarheSecond");
+            double DischarheSecond = double.Parse(Console.ReadLine());
+            Console.WriteLine("FireMode");
+            string FireMode = Console.ReadLine();
+
+
+            Weapons M16A = new Weapons(MagazineCapacity, MemberOfBulletsRemaining, DischarheSecond, FireMode); ; ; ; ;
+            Console.Clear();
+
+
+
+            Console.WriteLine("0- GetInfo");
+
+
+            while (true)
+            {
+
+                int choose = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (choose)
+                {
+                    case 0:
+                        Console.WriteLine("1- Shoot a bullet"); //Shoot metodu üçün
+                        Console.WriteLine("2 -The bullets in the magazine are fired "); //Fire metodu üçün
+                        Console.WriteLine("3- The number of bullets to fill the magazine completely"); //GetRemainBulletCount metodu üçün
+                        Console.WriteLine("4- refill the magazine"); //Reload metodu üçün
+                        Console.WriteLine("5- Change shooting mode"); //ChangeFireMode metodu üçün
+                        Console.WriteLine("6- Shortcut to stop the program"); //Proqramdan dayandırmaq üçün
+                        Console.WriteLine("7- Edit"); //Redaktə et
+                        Console.WriteLine("T- Say capacity"); //Tutumu dəyişsin
+                        Console.WriteLine("S- Number of bullets"); //Güllə sayı
+                        Console.WriteLine("D- Reset time"); //Sıfırlama saniyəsi 
+
+                        break;
+                    case 1:
+
+                        Console.WriteLine("1- Shoot a bullet");
+                        M16A.Shoot();
+                        break;
+                    case 2:
+
+                        Console.WriteLine("2 -The bullets in the magazine are fired ");
+                        M16A.Fire();
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("3- The number of bullets to fill the magazine completely");
+                        break;
+                    case 4:
+                        Console.WriteLine("4- refill the magazine");
+                        M16A.Reload();
+                        break;
+
+                    case 5:
+
+                        Console.WriteLine("5- Change Fire mode");
+                        M16A.ChangeFireMode();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("6- Shortcut to stop the program");
+
+                        break;
+                    case 7:
+                        Console.WriteLine("7- Edit");
+                        break;
+
+              
+                            //    break;
+                            //case T;
+                            //    Console.WriteLine("Tutumu dəyişsin");
+                            //    break;
+
+                            //    break;
+                            //case S;
+                            //    Console.WriteLine("Güllə sayı");
+                            //    break;
+
+                            //    break;
+                            //case D;
+                            //    Console.WriteLine("Sıfırlama saniyəsi ");
+                            //    break;
+
+
+                        }
+
+
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Farid111/Weapons.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Farid111
+{
+    public class Weapons
+    {
+        private int _magazineCapacity; //Darağın güllə tutumu
+        private int _memberOfBulletsRemaining; //Daraqdakı güllə sayı,
+        private float _discharheSecond; // Darağın boşalma saniyəsi
+        private string _fireMode; //atış modu
+
+
+        public Weapons(int magazineCapacity, int memberOfBulletsRemaining, double discharheSecond, string fireMode)
+        {
+            this.MagazineCapacity = MagazineCapacity;
+            this.MemberOfBulletsRemaining = MemberOfBulletsRemaining;
+            this.DischarheSecond = DischarheSecond;
+            this.FireMode = FireMode;
+        }
+
+        public int MagazineCapacity //Darağın güllə tutumu
+        {
+            get
+            {
+                return _magazineCapacity;
+            }
+            set
+            {
+                if (MagazineCapacity >= 0)
+
+                {
+                    _magazineCapacity = value;
+                }
+            }
+        }
+
+
+        public int MemberOfBulletsRemaining //Daraqdakı güllə sayı,
+        {
+            get
+            {
+                return _memberOfBulletsRemaining;
+            }
+            set
+            {
+                if (MemberOfBulletsRemaining >= 0)
+                {
+                    _memberOfBulletsRemaining = value;
+                }
+            }
+
+        }
+
+
+        public float DischarheSecond // Darağın boşalma saniyəsi
+        {
+            get
+            {
+                return _discharheSecond;
+            }
+            set
+            {
+                if (DischarheSecond > 0)
+                {
+                    _discharheSecond = value;
+                }
+            }
+        }
+
+        public string FireMode //atış modu
+        {
+
+            get
+            {
+                return _fireMode;
+            }
+            set
+            {
+                if (FireMode == "Single" && FireMode == "Auto")
+                    _fireMode = value;
+            }
+        }
+
+        public void ChangeFireMode() //Atış modunu dəyişir.
+        {
+            if (FireMode != "Auto")
+            {
+                Console.WriteLine("Auto");
+            }
+            else if (FireMode != "Single")
+            {
+                Console.WriteLine("Single");
+            }
+
+
+        }
+
+
+
+
+
+        public void Shoot()  // metodu hər çağırıldıqda 1 güllə atır(Ekranda bunu bildir)
+        {
+            if (MemberOfBulletsRemaining > 0)
+            {
+                MemberOfBulletsRemaining--;
+                Console.WriteLine("Shoot one bullet");
+
+            }
+            else
+                Reload();
+
+        }
+        public void Fire() //metodu daraqdakı güllələr hamısı atəşlənir və neçə saniyəyə bitdiyi qeyd olunur.
+        {
+
+            double time = DischarheSecond / MagazineCapacity * MemberOfBulletsRemaining;
+            Console.WriteLine(time + " : Time");
+        }
+
+
+        public int GetRemainBulletCount()  //darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+        {
+            int count = MagazineCapacity - MemberOfBulletsRemaining;
+
+            return count;
+        }
+
+
+
+
+        public void Reload() //darağı yenidən doldurur
+        {
+            if (MemberOfBulletsRemaining == MagazineCapacity)
+            {
+                Console.WriteLine("Don't reload");
+            }
+            else
+            {
+
+
+                Console.WriteLine("Reloding...");
+
+                Thread.Sleep((int)(1000 * 4));
+                Console.Clear();
+                Console.WriteLine("Reloded");
+            }
+
+
+
+            
+
+        }
+    }
+}
+
